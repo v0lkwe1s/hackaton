8ce54329ed06897a40778f6f921547b798b42727
williamvolkweis <williamvolkweis@volkweis>
2016-06-11 16:54:30 -0300

    quase la

--- js/cadastro-projetos-controller.js
+++ js/cadastro-projetos-controller.js
@@ -22,23 +22,4 @@ app.controller("cadastroProjetosCtrl", function($scope, $firebaseArray) {
 
 
 
-                <div class="col-sm-9 col-md-10 col-md-offset-2 main" >
-                    <div class="row">
-                        <div class="col-xs-12 col-md-12" ng-view>
-                            <div>
-                                <h1>Postar notícia</h1>
-                                <form>
-                                    <div class="form-group">
-                                        <label for="titulo">Título</label>
-                                        <input ng-model="titulo" type="text" class="form-control" id="titulo" placeholder="Título" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="descricao">Descrição</label>
-                                        <textarea ng-model="descricao" id="descricao" class="form-control" placeholder="Descrição" rows="6" required></textarea>
-                                    </div>
-                                    <button class="btn btn-default" ng-click="postar()">Postar</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
