43bea38b5d13f6d592cd5a3e13dbbf79adc567e9
Guilherme Guimarães <g-guimaraes@live.com>
2016-06-11 14:43:50 -0300

    Push funcionando.

--- js/cadastro-projetos-controller.js
+++ js/cadastro-projetos-controller.js
@@ -1,7 +1,7 @@
 var app = angular.module("app.up", ["firebase"]);
 app.controller("cadastroProjetosCtrl", function($scope, $firebaseArray) {
 
-   var ref = new Firebase("https://project-78368809104017595.firebaseio.com/results");
+   var ref = new Firebase("https://project-78368809104017595.firebaseio.com/projetos");
 
    $scope.titulo = '';
    $scope.descricao = '';
@@ -9,6 +9,12 @@ app.controller("cadastroProjetosCtrl", function($scope, $firebaseArray) {
    var projetos = $firebaseArray(ref);
 
    $scope.postar = function() {
-      projetos.push({ title: $scope.titulo,  description: $scope.descricao });
+      projetos.$add({ title: $scope.titulo,  description: $scope.descricao }).then(function(ref) {
+         var id = ref.key();
+         projetos.$indexFor(id);
+
+         $scope.titulo = '';
+         $scope.descricao = '';
+      });
    };
 });
